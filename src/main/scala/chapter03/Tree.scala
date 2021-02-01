3ef569c736f61744a312ccc0c74ee46e7c0277f6
Hiroaki Suzuki <rirazou05410@gmail.com>
2021-02-01 22:50:42 +0900

    WIP

--- src/main/scala/chapter03/Tree.scala
+++ src/main/scala/chapter03/Tree.scala
@@ -11,4 +11,37 @@ object Tree {
       case Leaf(_)      => 1
       case Branch(l, r) => 1 + size(l) + size(r)
     }
+
+  def maximum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(v)      => v
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+
+  def depth[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_)      => 0
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
+    }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    t match {
+      case Leaf(a)      => Leaf(f(a))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+
+  def fold[A, B](t: Tree[A])(lf: A => B)(bf: (B, B) => B): B =
+    t match {
+      case Leaf(a)      => lf(a)
+      case Branch(l, r) => bf(fold(l)(lf)(bf), fold(r)(lf)(bf))
+    }
+
+  def size2[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+  def maximum2(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def depth2[A](t: Tree[A]): Int = fold(t)(_ => 0)(1 + _ max _)
+
+  def map2[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
--- src/main/scala/chapter03/TreeApp.scala
+++
@@ -1,8 +0,0 @@
-package rirazou
-package chapter03
-
-object TreeApp extends App {
-  val tree = Branch(Branch(Leaf("A"), Leaf("b")), Branch(Leaf("c"), Leaf("d")))
-
-  println(Tree.size(tree))
-}
